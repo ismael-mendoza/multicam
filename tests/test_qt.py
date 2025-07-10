c0f6a4e03914787d576ff37fafc092382fae8b05
Ismael Mendoza <imendoza@umich.edu>
2025-07-10 14:43:29 -0500

    more testing

--- tests/test_qt.py
+++ tests/test_qt.py
@@ -1,11 +1,21 @@
 import numpy as np
 from numpy.testing import assert_allclose
+from scipy import stats
 
 from multicam.qt import qt, qt_gauss, qt_uniform
 
 
-def test_qts():
+def test_simple():
     x = np.array([1, 3, 2, 5, 4, 9, 11, 14, 17])
+    z = qt_gauss(x)
+    u = qt_uniform(x)
+
+    assert_allclose(x, qt(z, x))
+    assert_allclose(x, qt(u, x))
+
+
+def test_exponential():
+    x = stats.expon.rvs(size=(10000))
     z = qt_gauss(x)
     u = qt_uniform(x)
 
