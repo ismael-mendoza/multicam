a34d8de9ebf0dc6f0dad45c60e63fdcbbc6599ed
Ismael Mendoza <imendoza@umich.edu>
2025-08-06 13:50:59 -0500

    refactoring new functions for transformations based on another dist

--- multicam/qt.py
+++ multicam/qt.py
@@ -1,6 +1,7 @@
 """Our own implementation of quantile transformers."""
 
 import numpy as np
+from numpy import ndarray
 from scipy.stats import norm, rankdata
 
 
@@ -41,3 +42,33 @@ def qt_gauss(x, axis: int = 0, method="ordinal"):
     """Transform array to a Gaussian distribution based on ranks."""
     u = qt_uniform(x, axis=axis, method=method)
     return norm.ppf(u)
+
+
+# TODO: account for edges?
+# TODO: consider pre-sorted input.
+def qt_gauss_base(x: ndarray, x_base: ndarray):
+    """Gaussinize input based on another dataset."""
+    # always assume second dimension is n_features.
+    assert x.ndim == 2 and x_base.ndim == 2
+    assert x.shape[1] == x_base.shape[1]
+    n_features = x.shape[1]
+    x_gauss = np.zeros_like(x) * np.nan
+    for jj in range(n_features):
+        x_jj = x[:, jj]
+        xb_jj = np.sort(x_base[:, jj])  # required for np.interp
+        xb_gauss_jj = qt_gauss(xb_jj, method="ordinal")
+        x_gauss[:, jj] = np.interp(x_jj, xb_jj, xb_gauss_jj)
+    return x_gauss
+
+
+def qt_inverse_gauss_base(x_gauss: ndarray, x_base: ndarray):
+    assert x_gauss.ndim == 2 and x_base.ndim == 2
+    assert x_gauss.shape[1] == x_base.shape[1]
+    n_features = x_gauss.shape[1]
+    x_out = np.zeros_like(x_gauss) * np.nan
+    for jj in range(n_features):
+        xg_jj = x_gauss[:, jj]
+        xb_jj = np.sort(x_base[:, jj])  # required for np.interp
+        xb_gauss_jj = qt_gauss(xb_jj, method="ordinal")
+        x_out[:, jj] = np.interp(xg_jj, xb_gauss_jj, xb_jj)
+    return x_out
